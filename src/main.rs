b14f22bb177e67336f7fe8383286e08eef39ae30
Jason Leonhard <leonhard.jason@gmail.com>
2020-12-27 20:20:33 -0800

    using futures hyper and tokio_core we can make a http not https request

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
-// asynchronous example of tokio reqwest
-use std::collections::HashMap;
+extern crate futures;
+extern crate hyper;
+extern crate tokio_core;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-    println!("{:#?}", resp);
-    Ok(())
+use futures::{Future, Stream};
+use hyper::Client;
+use std::io::{self, Write};
+use tokio_core::reactor::Core;
+
+fn main() {
+    let mut core = Core::new().unwrap();
+    let client = Client::new(&core.handle());
+
+    // https appears not to work
+    let uri = "http://httpbin.org/get".parse().unwrap(); // works
+
+    let work = client.get(uri).and_then(|res| {
+        println!("Response: {}", res.status());
+
+        res.body()
+            .for_each(|chunk| io::stdout().write_all(&chunk).map_err(From::from))
+    });
+    core.run(work).unwrap();
 }
-// OUTPUT BELOW is also same as link above
-// {
-//     "origin": "37.120.149.92",
-// }
 
-// ref: https://github.com/seanmonstar/reqwest
+// Response: 200 OK
+// {
+//   "args": {},
+//   "headers": {
+//     "Host": "httpbin.org",
+//     "X-Amzn-Trace-Id": "Root=1-5fe95ac3-3cba9c8064c11382772cc264"
+//   },
+//   "origin": "37.120.149.92",
+//   "url": "http://httpbin.org/get"
+// }
